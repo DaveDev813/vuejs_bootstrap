103abca2704d526bdda84c03a8c06291c4b47a9d
Rodave Joseph B. Bobadilla <rbobadilla@isbx.com>
2018-08-14 16:43:46 +0800

    add typescript and configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,37 @@
-module.exports = {
+// module.exports = {
+//     module: {
+//       rules: [
+//         {
+//           test: /\.js$/,
+//           exclude: /node_modules/,
+//           use: {
+//             loader: "babel-loader"
+//           }
+//         }
+//       ]
+//     }
+//   };
+
+
+  const path = require('path');
+
+  module.exports = {
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
     module: {
       rules: [
         {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
         }
       ]
+    },
+    resolve: {
+      extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
     }
   };
